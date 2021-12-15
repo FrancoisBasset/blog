8ca4e115cd09cc3b159ea3ececa845ffed79b46a
FrancoisBasset <francois@local>
2021-12-15 14:04:56 +0100

    maj

--- migrations/Version20211213173815.php
+++ migrations/Version20211213173815.php
@@ -20,7 +20,7 @@ final class Version20211213173815 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL UNIQUE, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category_article (category_id INT NOT NULL, article_id INT NOT NULL, INDEX IDX_C5E24E1812469DE2 (category_id), INDEX IDX_C5E24E187294869C (article_id), PRIMARY KEY(category_id, article_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE category_article ADD CONSTRAINT FK_C5E24E1812469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE category_article ADD CONSTRAINT FK_C5E24E187294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE');
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,20 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryCreateType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
 	/**
-	 * @Route("/categories", name="categories")
+	 * @Route("/categories", name="categories", methods={"GET", "POST", "DELETE"})
 	 */
-    public function categories(CategoryRepository $categoryRepository): Response
+    public function categories(Request $request, CategoryRepository $categoryRepository, EntityManagerInterface $manager): Response
     {
+		$category = new Category();
+
+		$form = $this->createForm(CategoryCreateType::class, $category);
+		$form->handleRequest($request);
+
+		try {
+			if ($form->isSubmitted() && $form->isValid()) {
+				$manager->persist($category);
+				$manager->flush();
+			}
+		} catch (\Exception $e) {
+			$form->addError(new \Symfony\Component\Form\FormError($e->getMessage()));
+		}
+
         return $this->render('category/index.html.twig', [
-            'categories' => $categoryRepository->findAll()
+            'categories' => $categoryRepository->findAll(),
+			'form' => $form->createView()
         ]);
     }
 }
