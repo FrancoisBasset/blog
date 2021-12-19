00e0360eea23635b288851013e75aefdc5cf784a
FrancoisBasset <francois@local>
2021-12-19 13:29:08 +0100

    maj

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,10 +6,20 @@ use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 
 class CommentType extends AbstractType
 {
+	private $user;
+
+	public function __construct(Security $security)
+	{
+		$this->user = $security->getUser();
+	}
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -24,6 +34,14 @@ class CommentType extends AbstractType
 				'required' => false
 			])
         ;
+
+		$builder->addEventListener(FormEvents::POST_SET_DATA, function(FormEvent $event) {
+			$form = $event->getForm();
+
+			if ($this->user !== null) {
+				$form->get('author')->setData($this->user->getUserIdentifier());
+			}
+		});
     }
 
     public function configureOptions(OptionsResolver $resolver): void
